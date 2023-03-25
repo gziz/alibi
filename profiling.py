9097f09def332a0e3e7e5d09a4426bc2e99ab815
gziz <69287011+gziz@users.noreply.github.com>
2023-03-24 20:57:45 -0600

    Profiling

--- profiling.py
+++ profiling.py
@@ -11,7 +11,6 @@ args = parser.parse_args()
 seq_len = 512
 model_size = 100 # arbitrary
 
-alibi_gen = ALiBiAttentionMaskGenerator(num_heads=8)
 x = torch.empty((seq_len,model_size))
 
 for _ in range(args.loops):
